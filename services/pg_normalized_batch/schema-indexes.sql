dd89d4d02fdcf73bc7dd5d27bb345b4a609bc0e3
Sangeon Park <sangpark@g.hmc.edu>
2024-05-14 17:00:08 -0700

    finished normalized_batch

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,3 +1,3 @@
-CREATE INDEX idx_tags ON tweet_tags(tag);
-CREATE INDEX idx_id_tweets ON tweets(id_tweets);
-CREATE INDEX idx_text_fts on tweets USING gin(to_tsvector('english', text));
+CREATE INDEX ON tweet_tags(tag);
+CREATE INDEX ON tweets(id_tweets);
+CREATE INDEX ON tweets USING gin(to_tsvector('english', text));
